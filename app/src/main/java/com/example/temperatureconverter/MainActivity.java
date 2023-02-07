42bb03f0652d7d6dee030ef00200a8d7b7e84647
Jayden <jduncan9@stu.jsu.edu>
2023-02-07 17:39:44 -0600

    Screen layout complete, fToC() function implemented

--- app/src/main/java/com/example/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 
 import com.example.temperatureconverter.databinding.ActivityMainBinding;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -17,5 +21,50 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+
+                    if(binding.fahrenheitEditText.getText() != null){
+
+                        String fahrenheitText = binding.fahrenheitEditText.getText().toString();
+                        BigDecimal fahrenheitTemp = new BigDecimal(fahrenheitText);
+                        BigDecimal result = fToC(fahrenheitTemp);
+
+                        binding.celsiusEditText.setText(result.toString());
+
+                    }
+                    else{
+                        System.out.println("Fahrenheit Text View is empty");
+                    }
+
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private BigDecimal fToC(BigDecimal fTemp){
+        MathContext mc = new MathContext(3, RoundingMode.HALF_UP);
+
+        BigDecimal result1 = fTemp.subtract(new BigDecimal("32"), mc);
+
+        System.out.println(result1.toString());
+
+        BigDecimal finalResult = result1.multiply(new BigDecimal(String.valueOf(5/9.0)), mc);
+
+        System.out.println(finalResult.toString());
+
+        return finalResult;
+    }
+
+    private BigDecimal cToF(BigDecimal cTemp){
+        BigDecimal result = null;
+
+        return result;
     }
 }
